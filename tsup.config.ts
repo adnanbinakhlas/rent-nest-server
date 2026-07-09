80db9b2c35a3c33910441f84b88bae4c81003433
adnanbinakhlas <adnanbinakhlas.dev@gmail.com>
2026-07-09 17:30:34 +0600

    chore: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,28 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/server.ts"],
-  format: "esm",
+
+  format: ["esm", "cjs"],
+
   target: "esnext",
+
   outDir: "dist",
-  bundle: true,
-  splitting: false,
-  sourcemap: true,
+
   clean: true,
+
+  bundle: true,
+
+  splitting: false,
+
+  sourcemap: false,
+
+  banner: {
+    js: `
+
+   import { createRequire } from 'module';
+
+   const require = createRequire(import.meta.url);
+
+  `,
+  },
 });
